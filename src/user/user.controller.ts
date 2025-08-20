66a8cc5c524e7b0be7814e87fb7c08a48b85a2cf
Ricky Ng <ricky@aptika.co.id>
2025-08-20 18:10:59 +0700

    feat: create user controller sample http request

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('api/users')
-export class UserController {}
+export class UserController {
+  @Post()
+  post(): string {
+    return 'post users';
+  }
+
+  @Get('/sample')
+  get(): string {
+    return 'get user 222';
+  }
+}

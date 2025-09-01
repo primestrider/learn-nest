8099d8503739a5d8e5d516de2b4a34ddb8530bf6
Ricky <ricky@aptika.co.id>
2025-09-01 23:05:55 +0700

    http response

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,27 +1,44 @@
-import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Param,
+  Post,
+  Query,
+} from "@nestjs/common";
 
-@Controller('api/users')
+@Controller("api/users")
 export class UserController {
   @Post()
   post(): string {
-    return 'post users';
+    return "post users";
   }
 
-  @Get('/sample')
+  @Get("/sample")
   get(): string {
-    return 'get user 222';
+    return "get user 222";
   }
 
-  @Get('/sample/:id')
-  getDynamic(@Param('id') id: string): string {
+  @Get("/sample/:id")
+  getDynamic(@Param("id") id: string): string {
     return `GET SAMPLE ${id}`;
   }
 
-  @Get('/hello')
+  @Get("/hello")
   sayHello(
-    @Query('first_name') firstName: string,
-    @Query('last_name') lastName: string,
+    @Query("first_name") firstName: string,
+    @Query("last_name") lastName: string,
   ): string {
     return `Hello ${firstName} ${lastName}`;
+  }
+
+  @Get("/sample/response")
+  @Header("Content-Type", "application/json")
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    return {
+      data: "Sample Response JSON",
+    };
   }
 }
